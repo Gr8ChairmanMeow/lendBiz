0cf6f82df940126273a86faf7d7a13d261a57a8e
Javier Avitia <Javier.A.Avitia@gmail.com>
2017-10-05 17:50:11 -0700

    Competed first draft with JS form.

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,3 +1,25 @@
+var user = {};
+
+var nextForm = "Loan Amount:<br>"+
+	"<input type='text' name='loanAmt'>"+
+	"<br>"+
+	"Type of Business:<br>"+
+	"<input type='text' name='typeBiz'>"+
+	"<br>"+
+	"Years in Business:<br>"+
+	"<input type='text' name='yrsBiz'>"+
+	"<br>"+
+	"What do you need a loan for:<br>"+
+	"<input type='text' name='loanFor'>"+
+	"<br>"+
+	"Amount of Cash Reserves:<br>"+
+	"<input type='text' name='cashReserve'>"+
+	"<br>"+
+	"<div class='divButton' id='submitBtn'><strong>Submit</strong></div>"+
+	"<br>***We do not guantee that a loan will be underwritten.";
+
+var completeForm = "<div id='box'>Based on your selection you qualify for a business loan from the following underwriters:</div"
+
 $(".mIcon .divButton").on("click", function(){
 	var check = $("#divMenu").css("visibility");
 	if(check === "visible"){
@@ -7,3 +29,50 @@ $(".mIcon .divButton").on("click", function(){
     	$("#divMenu").css("visibility","visible");
     }
 });
+
+$("#nextBtn").on("click", function(){
+	var check = true;
+	var input = document.getElementById("myForm").elements;
+
+	for (var i = 0; i < input.length; i++) {
+		if(input[i].value === ""){
+			check = false;
+		}
+		else{
+			user[input[i].name] = input[i].value;
+		}
+	}
+
+	if(check){
+		document.getElementById("myForm").reset();
+		$("#myForm").html(nextForm);
+	}
+	else{
+		alert("Please complete form.");
+	}
+});
+
+$(document).on("click","#submitBtn", function(){
+	var check = true;
+	var input = document.getElementById("myForm").elements;
+
+	for (var i = 0; i < input.length; i++) {
+		if(input[i].value === ""){
+			check = false;
+		}
+		else{
+			user[input[i].name] = input[i].value;
+		}
+	}
+
+	if(check){
+		document.getElementById("myForm").reset();
+		console.log(user);
+		$("#applyForm").html(completeForm);
+	}
+	else{
+		alert("Please complete form.");
+	}	
+});
+
+/*<div class="divButton" id="submitBtn"><strong>Submit</strong></div>*/
